fbfdac29337ee04d78f1e6f17f86bef5de5c15eb
fcandes <fcandes@paterostechnologicalcollege.edu.ph>
2025-02-19 19:30:27 +0800

    Add files via upload

--- activity1.java
+++ activity1.java
@@ -1,9 +1,10 @@
 public class StudentNames {
     public static void main(String[] args) {
-        System.out.println("List of Students:");
+        System.out.println("List of Students;");
         // Students, add your names below using the format:
         // System.out.println("Your Name - SECTION");
         // Example:
         // System.out.println("Juan Dela Cruz - BSIT 2M");
+        System.out.println("Francis C. Andes - BSIT 2M")
     }
 }
